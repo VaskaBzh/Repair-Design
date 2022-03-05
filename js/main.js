7ae17079a6e6d3bcea22c13ceb004a6c5b76c3e8
Vasily <vasily-bazhenov.03@mail.ru>
2022-03-05 15:44:48 +0300

    react: opitimize project

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-/*
-document.addEventListener("DOMContentLoaded", function (event) {
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn = document.querySelector ('.modal__close')
-    const switchModal = () => {
-        modal.classList.toggle('modal--visible');
-    }
-    modalBtn.forEach(element => {
-        element.addEventListener('click', switchModal);
-    });
-    
-    closeBtn.addEventListener('click', switchModal)
-
-    document.addEventListener('click', (e) => {
-        if (e.target.classList === modal.classList) {
-            modal.classList.toggle('modal--visible');
-        };
-    });
-    
-    document.addEventListener('keydown', (e) => {
-        if(e.key == 'Escape') {
-            if (modal.classList.contains('modal--visible')) {
-                modal.classList.toggle('modal--visible');
-            };
-        };
-    });
-})
-*/
-
 $(document).ready(function () {
   var modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
